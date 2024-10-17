f209604e40909738a619649b9a157caaab0ea946
lotoknkn <pandapandamssp@gmail.com>
2024-10-17 14:18:52 +0900

    Create practiceA.java

---
+++ practiceA.java
@@ -0,0 +1,10 @@
+public Abstract class PracticeA{
+  private String name;
+  private int number;
+  public PracticeA(String name,int number){
+    this.name = name;
+    this.number = number;
+  }
+  public String getName(){return this.name;}
+  public int getNumber(){return this.number;}
+}
